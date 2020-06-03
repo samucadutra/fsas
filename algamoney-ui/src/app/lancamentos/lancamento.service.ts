4d77f0340e776f1f6e8b8f042bb8345aa031e299
Samuel Dutra <samuel.dutra@hotmail.com>
2020-06-03 09:23:04 -0300

    Módulo de lançamentos com paginação LAZY pronta

--- algamoney-ui/src/app/lancamentos/lancamento.service.ts
+++ algamoney-ui/src/app/lancamentos/lancamento.service.ts
@@ -2,28 +2,53 @@ import { Injectable } from '@angular/core';
 import { Http, Headers, URLSearchParams } from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
+import * as moment from 'moment';
 
-export interface LancamentoFiltro {
+export class LancamentoFiltro {
   descricao: string
+  dataVencimentoInicio: Date
+  dataVencimentoFim: Date
+  pagina = 0
+  itensPorPagina = 5
 }
 
 @Injectable()
 export class LancamentoService {
   lancamentosUrl = 'http://localhost:8080/lancamentos';
 
-  constructor(private http: Http) {}
+  constructor(private http: Http) { }
 
   pesquisar(filtro: LancamentoFiltro): Promise<any> {
     const params = new URLSearchParams();
     const headers = new Headers()
     headers.append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==')
 
-    if (filtro.descricao){
+    params.set('page', filtro.pagina.toString())
+    params.set('size', filtro.itensPorPagina.toString())
+
+    if (filtro.descricao) {
       params.set('descricao', filtro.descricao)
     }
+
+    if (filtro.dataVencimentoInicio) {
+      params.set('dataVencimentoDe', moment(filtro.dataVencimentoInicio).format('YYYY-MM-DD'))
+    }
+
+    if (filtro.dataVencimentoFim) {
+      params.set('dataVencimentoAte', moment(filtro.dataVencimentoFim).format('YYYY-MM-DD'))
+    }
+
     return this.http
       .get(`${this.lancamentosUrl}?resumo`, { headers, search: params })
       .toPromise()
-      .then((response) => response.json().content);
+      .then((response) => {
+        const responseJson = response.json()
+        const lancamentos = responseJson.content
+        const resultado = {
+          lancamentos: lancamentos,
+          total: responseJson.totalElements
+        }
+        return resultado
+      });
   }
 }
--- algamoney-ui/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ algamoney-ui/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,5 +1,10 @@
-import { LancamentoService } from './../lancamento.service';
 import { Component, OnInit } from '@angular/core';
+
+import { LazyLoadEvent } from 'primeng/components/common/api'
+
+import { LancamentoService, LancamentoFiltro } from './../lancamento.service';
+
+
 
 @Component({
   selector: 'app-lancamentos-pesquisa',
@@ -8,16 +13,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentosPesquisaComponent implements OnInit {
 
-  descricao: string
+  totalRegistros = 0
+  filtro = new LancamentoFiltro();
   lancamentos = [];
 
-  constructor(private lancamentoService: LancamentoService) {}
+  constructor(private lancamentoService: LancamentoService) { }
 
   ngOnInit() {
-    this.pesquisar();
+    // this.pesquisar();
   }
 
-  pesquisar() {
-    this.lancamentoService.pesquisar({ descricao: this.descricao }).then(lancamentos => this.lancamentos = lancamentos);
+  pesquisar(pagina = 0) {
+    this.filtro.pagina = pagina
+    this.lancamentoService
+      .pesquisar(this.filtro)
+      .then((resultado) => {
+        this.totalRegistros = resultado.total
+        this.lancamentos = resultado.lancamentos
+      });
+  }
+
+  aoMudarPagina(event: LazyLoadEvent) {
+    const pagina = event.first / event.rows
+    this.pesquisar(pagina)
   }
 }
